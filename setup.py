e0aded2f35714eac958ee54124ff7c4629a0c031
Thomas Roten <thomas@roten.us>
2016-03-31 21:59:53 -0500

    Bumps version to v0.1.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='yweather',
-    version='0.1',
+    version='0.1.1',
     author='Thomas Roten',
     author_email='thomas@roten.us',
     url='https://github.com/tsroten/yweather',
